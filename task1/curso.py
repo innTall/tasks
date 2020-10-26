739b9ba919b7efe7d46aba04c38434ab2eeb8eb3
innTall <troll999@hot.ee>
2020-10-26 11:54:53 +0100

    first

---
+++ task1/curso.py
@@ -0,0 +1,65 @@
+from random import randint, uniform
+import math
+# print(f'd = {d}')
+a = randint(0, 100)
+b = randint(0, 100)
+c = randint(0, 100)
+d = randint(0, 100)
+print(a, b, c, d)
+
+v = []
+u = []
+print('enter number x =  ')
+x = int(input())
+print('other y =  ')
+y = int(input())
+for i in range(10):
+  v = x * i
+  u = y + i
+  print(v, u)
+# if a < 50:
+#   print('number too small, need > 50')
+#   print('new number is: ')
+# if a > 50:
+#   print('ok')
+  
+
+# if a % 2 == 0 and a > 0:
+#   print(222222222)
+# elif a % 2 != 0 and a > 0:
+#   print(11111111)
+# elif a < 0 and b < 0:
+#   v = 3
+# elif a > 0 and b < 0:
+#   v = 4
+
+# elif a < b < c:
+#   v = l(b)
+# elif a == b:
+#   v = l(c)
+# else:
+#   v = 'no'
+
+#   a = b = 0
+#print(f'ind = {v}')
+#else:
+#  print(b, a)
+# x = 0
+# l = [a, b, c, d]
+# l.sort()
+# print(l[-1])
+# for i in l:
+#   if a > b: 
+#     print(f'a = {l[a]}')
+#   else:
+#     print(f'b = {l[b]}')
+
+# v = len(l)
+# y = v - x
+
+# print(a, b)
+# print(f'x = {x}, y = {y}')
+
+
+
+
